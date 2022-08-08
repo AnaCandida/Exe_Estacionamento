0526e1f8c1759ec5dccd0c9426e0ef9dd0863822
Ana Quadros <ana.quadros@cakeerp.com>
2022-08-07 21:16:21 -0300

    Inclusao de Veiculo tipo Moto no Estacionamento

--- funcionando.py
+++ funcionando.py
@@ -1,3 +1,4 @@
+
 class Veiculo:
     def __init__(self, placa):
         self.___placa = placa
@@ -26,7 +27,7 @@ class Veiculo:
 
     def sair_da_vaga(self):
         self.estacionado = False
-        print(f'saindo da vaga com o carro {self.placa}')
+        print(f'saindo da vaga com o veiculo {self.placa}')
 
     def __str__(self):
         tipo = 'Carro' if self.tipo == 2 else 'Moto'
@@ -54,37 +55,31 @@ class Moto (Veiculo):
 class Vaga:
     # 1 = moto
     # 2 = carro
-    id = 0
-    def __init__(self,  tamanho = 2, veiculo = Veiculo):
-        self._tipo = 2
+    def __init__(self, tipo, identificador, veiculo = Veiculo):
+        self._tipo = tipo
         self.livre = True
         self.placa = veiculo.placa
-    #   indenticador  
+        self.identificador = identificador 
 
     @property
     def tipo(self):
         return self._tipo
-
-    @tipo.setter
-    def tipo(self,valor):
-        if valor < 0 or valor > 2:
-            raise ValueError()
-        self._tipo = valor
-
+   
     def ocupar(self,veiculo = Veiculo):
 
-        # verificar se a vaga ta ocupada, verificar se ta ocupada ou nao
-        self.livre = False
-        if veiculo.tipo == 2 :
-            Vaga.tipo = 0
-        else:
-            Vaga.tipo = 1
+        # if self.livre == True:
         veiculo.estacionar(veiculo)
         print(f'Ocupando a vaga de tipo {self.tipo} com o veiculo tipo {veiculo.tipo} e a placa {self.placa}')
+        # else:
+        #     raise Exception('Vaga já está preenchida.')
 
     def desocupar(self):
-        self.livre = True
-        print(f'Desocupando a vaga')
+        
+        if self.livre == False:
+            self.livre = True
+            print(f'Desocupando a vaga')
+        else:
+            raise Exception('Vaga já está livre.')
 
 
     def __str__(self) -> str:
@@ -93,55 +88,79 @@ class Vaga:
 
 class Estacionamento():
  
-    vagas_em_uso = []
-    count = 0
-    
+    vagas_em_uso_carros = []
+    vagas_em_uso_motos = []
+    aguardando_carro = 0
+    aguardando_moto = 0    
     def __init__(self):
-        self.total_vagas = 10
+        self.total_vagas_carro = 10
+        self.total_vagas_moto = 10
+        self.total_vagas = self.total_vagas_carro + self.total_vagas_moto
         self.ultima_vaga_ocupada = 0
 
-    def estacionar(self,veiculo, vaga = Vaga,):
-        # instanciar vaga, comunicando vaga , indentificador
-
-        if self.ultima_vaga_ocupada < 50 and len(Estacionamento.vagas_em_uso) < 50:
+    def estacionar_carro(self,veiculo, vaga = Vaga,):
+        if len(Estacionamento.vagas_em_uso_carros) < self.total_vagas_carro:
             self.ultima_vaga_ocupada += 1
             vaga_usada = self.ultima_vaga_ocupada
+            v = Vaga(tipo =2 ,identificador = vaga_usada, veiculo = Veiculo)
+
+            if veiculo.tipo != 2:
+                raise Exception('Esta vaga aceita apenas carros.')
+            
             vaga.ocupar(veiculo)
             print(f'Estacionando o veiculo {veiculo.placa} na vaga {vaga_usada}')
             par = [veiculo.placa,vaga_usada]  
-            Estacionamento.vagas_em_uso.append(par)
-            return  print(f'estacionado {veiculo.placa} na vaga {vaga_usada}')
+            Estacionamento.vagas_em_uso_carros.append(par)
+            return  print(f'{veiculo.placa}estacionado na vaga {vaga_usada}')
         else:
-            Estacionamento.count += 1
-            return print(f'Estacionamento lotado, aguardando { Estacionamento.count}')
+            Estacionamento.aguardando_carro += 1
+            return print(f'Estacionamento sem vagas para carros, aguardando: { Estacionamento.aguardando_carro}')
 
-    def remover(self, veiculo, vaga = Vaga):
+    def remover_carro(self, veiculo, vaga = Vaga):
 
-        # print(self.vagas_em_uso[0].index(carro.placa))
-
-        for x  in Estacionamento.vagas_em_uso:
+        for x  in Estacionamento.vagas_em_uso_carros:
             try:
                 find = veiculo.placa in x
                 if find:
-                    index = Estacionamento.vagas_em_uso.index(x)
-                    self.vagas_em_uso.remove(x)
+                    index = Estacionamento.vagas_em_uso_carros.index(x)
+                    self.vagas_em_uso_carros.remove(x)
                     return print(f'Retirando veiculo {veiculo.placa}')
                     
             except Exception as e:
-                print('não está no estacionamento')
+                print('Não está no estacionamento')
 
-        # if self.ultima_vaga_ocupada > 0:
-        #     self.ultima_vaga_ocupada -= 1
-           
-        #     par = estacionamento.vagas_em_uso.index(veiculo.placa)
-        #     vaga.desocupar(veiculo)
-        #     print(f'Retirando o veiculo {veiculo.placa} da vaga {par}')
-        #     if veiculo.placa in estacionamento.vagas_em_uso:
-        #         estacionamento.vagas_em_uso.pop(estacionamento.vagas_em_uso.index(veiculo.placa))
-        #     return  print(f'estacionado {veiculo.placa} na vaga {par}')
-        # else:
-        #     return print('Estacionamento vazio')
 
+    def estacionar_moto(self,veiculo, vaga = Vaga,):
+            if len(Estacionamento.vagas_em_uso_motos) < self.total_vagas_moto:
+                self.ultima_vaga_ocupada += 1
+                vaga_usada = self.ultima_vaga_ocupada
+                v = Vaga(tipo =2 ,identificador = vaga_usada, veiculo = Veiculo)
+
+                if veiculo.tipo != 1:
+                    raise Exception('Esta vaga aceita apenas motos.')
+                
+                vaga.ocupar(veiculo)
+                print(f'Estacionando o veiculo {veiculo.placa} na vaga {vaga_usada}')
+                par = [veiculo.placa,vaga_usada]  
+                Estacionamento.vagas_em_uso_motos.append(par)
+                return  print(f'{veiculo.placa} estacionado na vaga {vaga_usada}')
+            else:
+                Estacionamento.aguardando_moto += 1
+                return print(f'Estacionamento sem vagas para motos, aguardando: { Estacionamento.aguardando_moto}')
+
+
+    def remover_moto(self, veiculo, vaga = Vaga):
+
+        for x  in Estacionamento.vagas_em_uso_motos:
+            try:
+                find = veiculo.placa in x
+                if find:
+                    index = Estacionamento.vagas_em_uso_motos.index(x)
+                    self.vagas_em_uso_motos.remove(x)
+                    return print(f'Retirando veiculo {veiculo.placa}')
+                    
+            except Exception as e:
+                print('Não está no estacionamento')
 
     def estado_do_estacionamento(self):
         print (f'----------------------------\n*** ESTACIONAMENTO INSTRUCT *** \n{self}\n----------------------------')
@@ -149,35 +168,35 @@ class Estacionamento():
 
     def __str__(self):  
 
-        total_ocupadas = int(len(Estacionamento.vagas_em_uso))
-        total_livres =  self.total_vagas - total_ocupadas
+        total_ocupadas_carros = int(len(Estacionamento.vagas_em_uso_carros))
+        total_ocupadas_motos = int(len(Estacionamento.vagas_em_uso_motos))
+        total_ocupadas = total_ocupadas_carros + total_ocupadas_motos
 
-        message1 = f'\n Vagas ocupadas: {total_ocupadas} Vagas livres: {total_livres} '
+        total_livres_carros =   self.total_vagas_carro - total_ocupadas_carros
+        total_livres_motos =  self.total_vagas_moto - total_ocupadas_motos
+        
+        message1 = f'\n Vagas ocupadas --> CARROS: {total_ocupadas_carros} MOTOS: {total_ocupadas_motos} \n Vagas livres --> CARROS {total_livres_carros} MOTOS:{total_livres_motos} '
 
-        message2 = f' Veiculos aguardando para estacionar: {self.total_vagas}'
+        message2 = f' Veiculos aguardando para estacionar: Carros:{ Estacionamento.aguardando_carro} Moto: {Estacionamento.aguardando_moto}'
 
+        if total_livres_carros == 0 and total_ocupadas < self.total_vagas:
+            return f'SEM VAGAS LIVRES PARA CARROS----TEMOS VAGAS MOTOS-------Carros aguardando para estacionar: { Estacionamento.aguardando_carro} {message1}'
+        if total_livres_motos == 0 and total_ocupadas < self.total_vagas:
+            return f'SEM VAGAS LIVRES PARA MOTOS----TEMOS VAGAS CARROS-------Motos aguardando para estacionar: { Estacionamento.aguardando_moto} {message1}'
 
         return ( '----LOTADO!----' + message2 + message1 ) if total_ocupadas == self.total_vagas else ( '----TEMOS VAGAS----' + message1)
 
 
-
 e = Estacionamento()
-v = Vaga()
 
 carroAna = Carro(1212)
 carroBia = Carro(5050)
-moto1 = Moto(8989)
+motoTiago = Moto(8989)
 
-e.estacionar(carroAna)
-e.estacionar(carroBia)
+e.estacionar_carro(carroAna)
+e.estacionar_carro(carroBia)
+e.estacionar_moto(motoTiago)
 
-e.estado_do_estacionamento()
-
-e.remover(carroAna)
-
-e.estado_do_estacionamento()
-
-e.estacionar(moto1)
 e.estado_do_estacionamento()
 
 
